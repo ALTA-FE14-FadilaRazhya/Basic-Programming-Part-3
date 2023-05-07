5145c99451d07eef66b982719f8ead1f65ef910f
fadilarazhya <fadilarazhya03@gmail.com>
2023-05-08 00:58:47 +0800

    edit problem2

--- problem2.js
+++ problem2.js
@@ -5,9 +5,5 @@ function joinArrayRemoveDuplicate(arrayA, arrayB) {
   }
   
   console.log(joinArrayRemoveDuplicate(["apel", "anggur"], ["lemon", "leci", "nanas"]));
-  
-  
   console.log(joinArrayRemoveDuplicate(["samsung", "apple"], ["apple", "sony", "xiaomi"]));
-  
-  
   console.log(joinArrayRemoveDuplicate(["football", "basketball"], ["basketball", "football"]));
